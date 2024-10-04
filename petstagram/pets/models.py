0efdc19a7bf81af7b7cae56fcbbdbe157178c27e
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-10-04 13:35:45 +0300

    Added last changes i did at school

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -7,12 +7,16 @@ class Pet(models.Model):
     name = models.CharField(max_length=50)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(unique=True, null=False, blank=True)    # new
+    slug = models.SlugField(unique=True, null=False, blank=True, editable=False)    # new
 
     def save(self, *args, **kwargs):
         # Промяна
+        super().save(*args, **kwargs)
         if not self.slug:
             self.slug = slugify(f"{self.name}-{self.id}")
 
         # Запазване в базата
-        super().save(*args, **kwargs)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
