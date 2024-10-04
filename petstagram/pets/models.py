1250935930564fee2e61851bf8d9698672c61b21
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-10-04 09:46:38 +0300

    Add files via upload
    Save from yesterday night

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    personal_photo = models.URLField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    slug = models.SlugField(unique=True, null=False, blank=True)    # new
+
+    def save(self, *args, **kwargs):
+        # Промяна
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        # Запазване в базата
+        super().save(*args, **kwargs)
--- petstagram/settings.py
+++ petstagram/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -129,7 +133,7 @@ STATICFILES_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
