026c0ab8756cd24b0c2ae4b489a50c9dd1777b56
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-10-18 17:48:27 +0300

    All code since the end of workshop-part2, finally written and tested

---
+++ petstagram/photos/views.py
@@ -0,0 +1,53 @@
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, redirect
+
+from petstagram.common.forms import CommentForm
+from petstagram.photos.forms import PhotoCreateForm, PhotoEditForm
+from petstagram.photos.models import Photo
+
+
+def photo_add_page(request):
+    form = PhotoCreateForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    context = {'form': form}
+    return render(request, 'photos/photo-add-page.html', context)
+
+
+def photo_edit_page(request, pk: int):
+    photo = Photo.objects.get(pk=pk)
+    form = PhotoEditForm(request.POST or None, instance=photo)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('photo-details', pk)
+
+    context = {
+        'form': form,
+        'photo': photo,
+    }
+    return render(request, 'photos/photo-edit-page.html', context)
+
+
+def photo_details_page(request, pk: int):
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+    comment_form = CommentForm()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments,
+        'comment_form': comment_form,
+    }
+
+    return render(request, 'photos/photo-details-page.html', context=context)
+
+def photo_delete_page(request, pk: int):
+    photo = Photo.objects.get(pk=pk)
+    photo.delete()
+    return redirect('home')
+
