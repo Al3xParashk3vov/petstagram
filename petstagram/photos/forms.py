a524dbd09fd628b195725e3439299b8d532eafb8
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-10-19 16:32:16 +0300

    Workshop-part3 done. Added class based views to photos

--- petstagram/photos/forms.py
+++ petstagram/photos/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from petstagram.photos.models import Photo
 
 
-class PhotoCreateForm(forms.ModelForm):
+class PhotoAddForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = '__all__'
--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,35 +1,42 @@
+from audioop import reverse
 from lib2to3.fixes.fix_input import context
 
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DetailView
 
 from petstagram.common.forms import CommentForm
-from petstagram.photos.forms import PhotoCreateForm, PhotoEditForm
+from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
 
 
-def photo_add_page(request):
-    form = PhotoCreateForm(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        form.save()
-        return redirect('home')
-    context = {'form': form}
-    return render(request, 'photos/photo-add-page.html', context)
+class PhotoAddPage(CreateView):
+    model = Photo
+    template_name = 'photos/photo-add-page.html'
+    form_class = PhotoAddForm
+    success_url = reverse_lazy('home')
 
 
-def photo_edit_page(request, pk: int):
-    photo = Photo.objects.get(pk=pk)
-    form = PhotoEditForm(request.POST or None, instance=photo)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('photo-details', pk)
+class PhotoEditPage(UpdateView):
+    model = Photo
+    template_name = 'photos/photo-edit-page.html'
+    form_class = PhotoEditForm
 
-    context = {
-        'form': form,
-        'photo': photo,
-    }
-    return render(request, 'photos/photo-edit-page.html', context)
+    def get_success_url(self):
+        return reverse_lazy('photo-details', kwargs={'pk': self.object.pk})
 
+
+class PhotoDetailsView(DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['likes'] = self.object.like_set.all()
+        context['comments'] = self.object.comment_set.all()
+        context['comment_form'] = CommentForm()
+        return context
 
 def photo_details_page(request, pk: int):
     photo = Photo.objects.get(pk=pk)
